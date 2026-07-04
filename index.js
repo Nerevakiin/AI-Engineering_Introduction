7051d9db2d37b1686866bab7a73b0068ac311084
Nerevakiin <djmatisk@yahoo.gr>
2026-07-05 00:40:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import "dotenv/config"
+import { checkEnvironment } from "./utils.js"
+import OpenAI from 'openai'
+
+
+const openai = new OpenAI({
+    apiKey: process.env.AI_KEY,
+    baseURL: process.env.AI_URL,
+    dangerouslyAllowBrowser: true
+})
+
+checkEnvironment()
+
+// const userPrompt = "Suggest some gifts for someone who loves outsider music like Jandek for example. Don't ask any follow-up questions. Use less than 100 words."
+
+// const userMessage = {
+//     role: "user",
+//     content: userPrompt
+// }
+
+const response = await openai.chat.completions.create({
+    model: process.env.AI_MODEL,
+    messages: [{
+        role: "user",
+        content: "Suggest some gifts for someone who loves black metal. Don't ask any follow-up questions. Use less than 100 words."
+    }]
+    // max_completion_tokens: 256
+})
+
+console.log(response.choices[0].message.content)
